c891c5c9f30d0f8080585d5a12319a3428b015e4
Dineysd <dineysd@hotmail.com>
2023-02-28 00:01:01 -0300

    add paginacao padrao no medico e pacientes

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -33,7 +34,7 @@ public class PacienteController {
 	}
 	
 	@GetMapping
-    public Page<DadosListagemPaciente> listar(Pageable paginacao) {
+    public Page<DadosListagemPaciente> listar(@PageableDefault(size=10, sort= {"nome"})Pageable paginacao) {
         return repository.findAll(paginacao).map(DadosListagemPaciente::new);
     }
 
