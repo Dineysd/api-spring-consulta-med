c609608a16d4d1e7866da626a48e08e87dd171a5
Dineysd <dineysd@hotmail.com>
2023-03-05 09:24:57 -0300

    add modelo, repository e controller Consulta

---
+++ src/main/java/med/voll/api/consulta/ConsultaRepository.java
@@ -0,0 +1,10 @@
+package med.voll.api.consulta;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import med.voll.api.model.Consulta;
+
+public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
+
+
+}
